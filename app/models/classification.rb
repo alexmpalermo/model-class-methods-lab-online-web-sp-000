0044640748bf5e7153cf7e6fea6129e294ea29e0
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-18 18:21:13 +0000

    Automatically backed up by Learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,12 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+  
+  def my_all
+    all classifications
+  end
+  
+  def self.longest
+    classifications for longest boat 
+  end
 end
