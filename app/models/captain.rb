b30ca4fde99ef5d68c4b7508fd387b2d7c0a9709
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-18 18:38:21 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,7 +2,7 @@ class Captain < ActiveRecord::Base
   has_many :boats
   
   def self.catamaran_operators
-    
+    joins(boats: :classifications).where(classifications: {name: "Catamaran"})
   end 
   
   def self.sailors
