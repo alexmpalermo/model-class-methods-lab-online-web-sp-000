621e664a02939c8d8bfa1dc9d793f1dc543b573c
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-18 18:36:36 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,18 +2,18 @@ class Captain < ActiveRecord::Base
   has_many :boats
   
   def self.catamaran_operators
-    all captains of catamarans
+    #all captains of catamarans
   end 
   
   def self.sailors
-    all captains w sailboats
+    #all captains w sailboats
   end
   
   def self.talented_seafarers
-    captains of motorboats and sailboats
+    #captains of motorboats and sailboats
   end 
   
   def self.non_sailors
-    people not captains of sailboats
+    #people not captains of sailboats
   end
 end
