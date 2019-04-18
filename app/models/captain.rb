552f2921d62221c8c33e4ffae5331f85dc87b5c9
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-18 18:45:01 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -18,6 +18,6 @@ class Captain < ActiveRecord::Base
   end 
   
   def self.non_sailors
-    joins(boats: :classifications).where.not(classifications: {name: "Sailboat"})
+    where.not(self.sailors)
   end
 end
