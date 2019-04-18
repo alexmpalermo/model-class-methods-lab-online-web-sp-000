b24b7f7955ee805aa938780d3d984c638ce6d282
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-18 18:26:10 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,7 +4,7 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
   
   def self.first_five
-    where(boat: boat_id) 
+    limit(5) 
   end 
   
   def self.dinghy
