f4f5a164e9f0fb03f7aafa654fcf54c3dfbf4d07
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-18 18:18:34 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,32 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+  
+  def first_five
+    first 5 boats 
+  end 
+  
+  def dinghy
+    shorter than 20ft
+  end 
+  
+  def ship
+    20ft or longer
+  end 
+  
+  def last_three_alphabetically
+    last 3 boats in order 
+  end
+  
+  def without_a_captain
+    
+  end
+  
+  def sailboats
+    all boats that are sailboats
+  end
+  
+  def with_three_classifications
+    all boats with 3 classifications 
+  end 
 end
