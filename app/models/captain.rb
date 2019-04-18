3d8823e35f4c183c98789ca10ce10a3531c9d02e
Alexandra Palermo <alexmpalermo@gmail.com>
2019-04-18 18:38:47 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -6,7 +6,7 @@ class Captain < ActiveRecord::Base
   end 
   
   def self.sailors
-    
+    joins(boats: :classifications).where(classifications: {name: "Sailboat"})
   end
   
   def self.talented_seafarers
